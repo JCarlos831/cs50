ad053511b4c89b6e948c116e30209014b145391c
Juan Montoya <jcmontoya@intracitygeeks.org>
2018-05-08 13:13:00 +0000

    Added while loop and nested if else statements. Code is incomplete and currently not compiling.

--- week3/pset4/recover/recover.c
+++ week3/pset4/recover/recover.c
@@ -10,31 +10,66 @@ int main (int argc, char *argv[])
         return 1;
     }
 
-    char *infile = argv[1];
+    char *sdCard = argv[1];
 
-    FILE *inptr = fopen(infile, "r");
+    FILE *sdCardPtr = fopen(sdCard, "r");
 
-    if (inptr == NULL)
+    if (sdCardPtr == NULL)
     {
-        fprintf(stderr, "Could not open %s.\n", infile);
+        fprintf(stderr, "Could not open %s.\n", sdCard);
         return 2;
     }
 
-    int buffer[4];
+    // size of each block
+    int size = 512;
 
-    fread(buffer, 512, 1, inptr);
+    // set buffer array
+    int buffer[512];
 
-    if (buffer[0] == 0xff &&
-        buffer[1] == 0xd8 &&
-        buffer[2] == 0xff &&
-        (buffer[3] & 0xf0) == 0xe0)
-        {
-            printf("It's a true");
-            sprintf(filename, "%03i.jpg", 2)
-            FILE *img = fopen(filename, "w");
+   // jpeg counter
+    int jpeg = 0;
+
+    char *newCard = NULL;
+
+    // while reading each 512 byte block...
+    while (fread(buffer, size, 1, sdCardPtr)) {
+        // check for a jpg by matching the first 4 bytes of a block
+        // is this the start of a new jpg?
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+            {
+                printf("It's a match");
+
+                // sprintf(infile, "%03i.jpg", jpeg);
+                sprintf(newCard, "%03i.jpg", jpeg);
+
+                FILE *img = fopen(newCard, "w");
+
+                fwrite(sdCardPtr, size, 1, img);
+                jpeg++;
+            }
+            else
+            {
+
+            }
+
+                // if a jpeg has already been found and then another is found
+                if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+                {
+                    fclose(img);
+
+                    FILE *img = fopen(newCard, "w");
+
+                    fopen(newCard, "w");
+
+                    sprintf(newCard, "%03i.jpg", jpeg);
+
+                    fwrite(sdCardPtr, 512, 1, img);
+                    jpeg++;
+                    fclose(img);
+                }
         }
 
-    fclose(inptr);
+    fclose(sdCardPtr);
 
     return 0;
 }
