0c1cf34bdc1fd377fae1a5f8b5f89aaff3d2e051
Juan Montoya <jcmontoya@intracitygeeks.org>
2018-05-16 03:37:26 +0000

    Speller working. Need to add comments.

--- week4/pset5/speller/dictionary.c
+++ week4/pset5/speller/dictionary.c
@@ -1,14 +1,19 @@
 // Implements a dictionary's functionality
 
-#include <cs50.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
+// define max size of hashtable. this number is a constant.
+// tried several numbers, this seemed to be the fastest.
+#define HASHTABLE_SIZE 66500
+
+// define a node structure
 typedef struct node
 {
     char word[LENGTH + 1];
@@ -16,24 +21,50 @@ typedef struct node
 }
 node;
 
-// node *node1 = malloc(sizeof(node));
+node *hashtable[HASHTABLE_SIZE] = {NULL};
 
-int HASHTABLE_SIZE = 65536;
+node *head = NULL;
+
+// Need a word counter to use in size
+int wordCounter = 0;
+
+// hash function
+// Credit to Neel Mehta https://github.com/hathix/cs50-section/blob/master/code/7/sample-hash-functions/good-hash-function.c
+unsigned int hash_word(const char *word)
+{
+    unsigned long hash = 5381;
+
+    for (const char *ptr = word; *ptr != '\0'; ptr++)
+    {
+        hash = ((hash << 5) + hash) + tolower(*ptr);
+    }
+
+    return hash % HASHTABLE_SIZE;
+}
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
     // TODO
 
-    // hashtable[hash(word)];
+    // Assigning what bucket the word will fall into
+    unsigned int hash = hash_word(word);
 
-    // node *cursor = node1;
-    // while (cursor != NULL)
-    // {
-    //     // do something
-    //     // strcmp?
-    //     cursor = cursor->next;
-    // }
+    // cursor will point to bucket the word will be in
+    node *cursor = hashtable[hash];
+
+    while (cursor != NULL)
+    {
+        // do something
+        if ((strcasecmp(cursor->word, word)) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            cursor = cursor->next;
+        }
+    }
 
     return false;
 }
@@ -43,8 +74,17 @@ bool load(const char *dictionary)
 {
     // TODO
     FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        printf("Could not open dictionary\n");
+        return false;
+    }
 
-    while (fscanf(file, "%s\n", word) != EOF)
+    // Create char * array pointer to store a word in
+    char word[LENGTH + 1];
+
+
+    while (fscanf(file, "%s ", word) != EOF)
     {
         node *new_node = malloc(sizeof(node));
         if (new_node == NULL)
@@ -54,47 +94,44 @@ bool load(const char *dictionary)
         }
         else
         {
-            // hash function
-            // credit to delipity at https://www.reddit.com/r/cs50/comments/1x6vc8/pset6_trie_vs_hashtable/#bottom-comments
-            int hash_it(char* word)
-            {
-                unsigned int hash = 0;
-                for (int i=0, n=strlen(word); i<n; i++)
-                    hash = (hash << 2) ^ tolower(word[i]);
-                return hash % HASHTABLE_SIZE;
-            }
-
-            node *hashtable[HASHTABLE_SIZE];
-
             strcpy(new_node->word, word);
-            new_node->next = node1;
-            node1 = new_node;
+            // if this node is the last node, point to NULL
+            new_node->next = NULL;
+
+            unsigned int hash = hash_word(word);
+
+            new_node->next = hashtable[hash];
+            hashtable[hash] = new_node;
+
+            wordCounter++;
         }
     }
     fclose(file);
     return true;
-
-
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return wordCounter;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
     // TODO
-    // node *cursor = node1;
 
-    // while (cursor != NULL)
-    // {
-    //     node *temp = cursor;
-    //     cursor = cursor->next;
-    //     free(temp);
-    // }
-    // return false;
+    for (int i = 0; i < HASHTABLE_SIZE; i++)
+    {
+        node *cursor = hashtable[i];
+
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+    }
+    return true;
 }
